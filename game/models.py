dabc04aa0f5d6bbcd15a1ff4ddb4a4947f6bcf74
AnaMelisaGo <melisago.rockn@gmail.com>
2025-04-27 19:18:55 +0000

    Create models for the game and make migrations

--- game/models.py
+++ game/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+from playlists.models import Playlist
 
-# Create your models here.
+
+class Game(models.Model):
+    """ Game model """
+    played_on = models.DateTimeField(auto_now_add=True)
+    game_master = models.ForeignKey(User, on_delete=models.CASCADE, related_name='game')
+    playlist = models.ForeignKey(Playlist, on_delete=models.CASCADE)
+
+    class Meta:
+        """ Display the playlist in ascending order according to the date created """
+        ordering = ['-played_on']
+
+    def __str__(self):
+        """ Function to return the dates of the game played and the game_master """
+        return f"Played {self.playlist} on {self.played_on} by {self.game_master}"
+
+
+class Winner(models.Model):
+    """ Winner model """
+    game = models.ForeignKey(Game, on_delete=models.CASCADE, related_name='winner')
+    name = models.CharField(max_length=255)
+    prize = models.CharField(max_length=255)
+    winning_numbers = models.JSONField(default=list)
