a76d43093e1e70a096efc11eef91f952c5680c06
AnaMelisaGo <melisago.rockn@gmail.com>
2025-04-29 19:16:10 +0000

    Add next button function to randomize numbers of the songs playlists

--- game/urls.py
+++ game/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('game_board/<int:playlist_id>/<slug:slug>/', views.start_game, name='start_game'),
-    path('next_number/<int:playlist_id>/', views.next_number, name='next_number'),
+    path('start_gameboard/<int:playlist_id>/<slug:slug>/', views.start_gameboard, name='start_gameboard'),
+    path('next_number/', views.next_number, name='next_number'),
 ]
--- game/views.py
+++ game/views.py
@@ -5,8 +5,8 @@ from .models import Game
 import random
 
 @login_required
-def start_game(request, playlist_id, slug):
-    """ game """
+def start_gameboard(request, playlist_id, slug):
+    """ Game board to play music bingo """
     playlist = Playlist.objects.get(pk=playlist_id, slug=slug, game_master=request.user)
     songs = Song.objects.filter(playlist=playlist)
     request.session['playlist_id'] = playlist_id
@@ -16,7 +16,7 @@ def start_game(request, playlist_id, slug):
     request.session['prizes'] = ['Line Down', 'Line Across', 'Four Corners', 'Full House']
     request.session['available_prizes'] = []
 
-    prizes = request.session.get('prizes')
+    prizes = request.session.get('prizes', [])
     
     # game = Game.objects.create(game_master=request.user, playlist=playlist)
     return render(request, 'game/game_board.html', {
@@ -27,30 +27,39 @@ def start_game(request, playlist_id, slug):
 
 
 @login_required
-def next_number(request, playlist_id):
+def next_number(request):
+    """ Function to scramble the numbers and push the numbers to the variables and
+    pass them to the game board """
+
+    if 'called_numbers' not in request.session:
+        request.session['called_numbers'] = []
+
+    playlist_id = request.session.get('playlist_id')
     playlist = Playlist.objects.get(pk=playlist_id, game_master=request.user)
     songs = Song.objects.filter(playlist=playlist)
 
-    called_numbers = []
     all_numbers = list(songs.values_list('number', flat=True))
-    remaining_number = list(set(all_numbers) - set(called_numbers))
-    p = called_numbers[::-5]
-    previous_numbers = p[:-1]
-    current_number = None
+    called_numbers = request.session.get('called_numbers', [])
+    remaining_numbers = list(set(all_numbers) - set(called_numbers))
+    current_number = request.session.get('current_number')
 
-    if remaining_number:
-        next_number = random.choice(remaining_number)
-        called_numbers.append(current_number)
+    #p = called_numbers[::-5]
+    #previous_numbers = p[:-1]
+    #current_number = None
+
+    if remaining_numbers:
+        next_number = random.choice(remaining_numbers)
+        called_numbers.append(next_number)
         current_number = next_number
+    #else
 
-    return redirect('game_board', playlist_id=playlist.id, slug=playlist.slug)
+    return redirect('start_gameboard', playlist_id=playlist.id, slug=playlist.slug)
 
-    """ return render(request, 'game/game_board.html', {
+    """ 
+    
+    return render(request, 'game/game_board.html', {
             'called_numbers': called_numbers,
             'all_numbers': all_numbers,
             'previous_numbers': previous_numbers,
             'current_number': current_number,
         }) """
-    # TESTING END ---------------------------------------------
-
-
