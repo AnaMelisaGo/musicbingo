66367df76da00efaf5dcf4ef271c32bc777d1fc0
AnaMelisaGo <melisago.rockn@gmail.com>
2025-04-29 18:46:28 +0000

    Set up game board with the playlist data and to store data in the session

--- game/urls.py
+++ game/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('game_board/<int:playlist_id>/<slug:slug>/', views.game_board, name='game_board'),
+    path('game_board/<int:playlist_id>/<slug:slug>/', views.start_game, name='start_game'),
+    path('next_number/<int:playlist_id>/', views.next_number, name='next_number'),
 ]
