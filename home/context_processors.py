1fccd186bbaf3da9a9a9be1100edb16f056d8e97
AnaMelisaGo <melisago.rockn@gmail.com>
2025-05-17 13:49:09 +0100

    Make changes in context

--- home/context_processors.py
+++ home/context_processors.py
@@ -4,7 +4,6 @@ from playlists.models import Playlist
 def global_context(request):
     """ A global context to pass the models accross the entire application """
     if request.user.is_authenticated:
-        print("Logged in user:", request.user)
         playlists = Playlist.objects.filter(game_master=request.user)
         return {'playlists': playlists}
     return {
