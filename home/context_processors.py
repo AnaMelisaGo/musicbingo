1606bd2fd87c25d0eda69597de1d30409622be2e
AnaMelisaGo <melisago.rockn@gmail.com>
2025-05-17 16:04:22 +0100

    Change context.py to be rendered and use correctly

--- home/context_processors.py
+++ home/context_processors.py
@@ -4,8 +4,12 @@ from playlists.models import Playlist
 def global_context(request):
     """ A global context to pass the models accross the entire application """
     if request.user.is_authenticated:
-        playlists = Playlist.objects.filter(game_master=request.user)
-        return {'playlists': playlists}
+        user_playlists = Playlist.objects.filter(game_master=request.user)
+        return {
+            'user_playlists': user_playlists,
+            'can_add_playlist': user_playlists.count() < 5,
+            }
     return {
-        'playlists': [],
+        'user_playlists': [],
+        'can_add_playlist': True,
     }
