374c8d2a6f866bd829876851516fae36107e47b8
AnaMelisaGo <melisago.rockn@gmail.com>
2025-05-19 12:29:29 +0100

    Set up for deployment

--- musicbingo/settings.py
+++ musicbingo/settings.py
@@ -12,21 +12,24 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Environment
+ENVIRONMENT = os.getenv('ENVIRONMENT', 'development') # 'development' or 'production'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*0_1f@3e_-v^7k=v#5%7ud2s3#&)5sdkvz+e0020drt9_$f-(w'
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-*0_1f@3e_-v^7k=v#5%7ud2s3#&)5sdkvz+e0020drt9_$f-(w')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost 127.0.0.1').split()
 
 
 # Application definition
@@ -119,12 +122,17 @@ CSRF_TRUSTED_ORIGINS = [
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if ENVIRONMENT == 'production':
+    DATABASES = {
+        'default': dj_database_url.config(default=os.getenv('DATABASE_URL'), conn_max_age=600)
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
@@ -162,8 +170,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+# The URL prefix for static files served from STATIC_ROOT. This is the URL that will be used to access the static files in production.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+# Media files (user-uploaded files)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR/ 'media'
 
---
+++ staticfiles/js/script.js
@@ -0,0 +1,157 @@
+// TOAST
+document.addEventListener('DOMContentLoaded', function () {
+    const toastElList = document.querySelectorAll('.toast');
+    const toastList = [...toastElList].map(toastEl => new bootstrap.Toast(toastEl));
+    toastList.forEach(toast => toast.show());
+});
+
+// playlist modal
+document.addEventListener('DOMContentLoaded', function() {
+    const urlParams = new URLSearchParams(window.location.search);
+    if (urlParams.get('open_modal') === '1') {
+        const deleteModalButton = document.getElementById('deleteModalButton');
+        deleteModalButton.click();
+    }
+    if (urlParams.get('open_modal') === '2') {
+        const backButton = document.getElementById('backButtonModal');
+        backButton.click();
+    }
+    if (urlParams.get('open_modal') === '3') {
+        const backGame = document.getElementById('backGameModal');
+        backGame.click();
+    }
+});
+
+// prize button
+document.querySelectorAll('.prize-btn').forEach(button => {
+    button.addEventListener('click', function() {
+        const prize = this.getAttribute('data-prize');
+        document.getElementById('prizeInput').value = prize;
+    });
+});
+
+
+// Checking winning numbers
+const selectedNumbers = new Set();
+
+document.querySelectorAll('.winning-btn').forEach(button => {
+    button.addEventListener('click', () => {
+        const number = button.getAttribute('data-number');
+
+        if (selectedNumbers.has(number)) {
+            selectedNumbers.delete(number);
+            button.classList.remove('btn-success');
+            button.classList.add('btn-secondary');
+        }
+        else {
+            selectedNumbers.add(number);
+            button.classList.remove('btn-secondary');
+            button.classList.add('btn-success');
+        }
+
+        document.getElementById('selected_numbers_input').value = JSON.stringify(Array.from(selectedNumbers));
+    });
+});
+
+// canvas confetti home button
+
+document.addEventListener('DOMContentLoaded', function() {
+    const handleConfetti = () => {
+        confetti({
+            particleCount: 100,
+            spread: 100,
+            scalar: 2,
+        });
+    }
+
+    const winnerConfetti = () => {
+        confetti({
+            particleCount: 700,
+            spread: 500,
+        });
+    }
+
+    const homeButton = document.querySelector('#home-button');
+    if (homeButton) {
+        homeButton.addEventListener('click', handleConfetti);
+    }
+
+    const checkWinner = document.querySelector('#check-winner');
+    if (checkWinner) {
+        checkWinner.addEventListener('click', winnerConfetti);
+    }
+});
+
+// trash button
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll('.delete-video-btn').forEach(function(button) {
+        button.addEventListener('click', function() {
+            const index = this.getAttribute('data-index');
+            const input = document.getElementById('clear_video_' + index);
+            if (input) {
+                input.value = 'true';
+                const previewCell = this.closest('td').previousElementSibling;
+                previewCell.innerHTML = '<small>Video removed</small>';
+                previewCell.classList.add('text-muted');
+                this.remove();
+            }
+        });
+    });
+});
+
+// To display video file name when selected
+document.addEventListener('DOMContentLoaded', function(){
+    document.querySelectorAll('input[type="file"]').forEach(input => {
+        input.addEventListener('change', function () {
+            const fileNameDisplay = this.closest('td').querySelector('.file-name-display');
+            if (this.files.length > 0) {
+                fileNameDisplay.textContent = this.files[0].name;
+            } else {
+                fileNameDisplay.textContent = '';
+            }
+        });
+    });
+});
+
+// auto-play button
+document.addEventListener('DOMContentLoaded', function() {
+    const autoPlayCheckbox = document.getElementById('autoCheckbox');
+    const video = document.getElementById('videoPlayer');
+    const nxtBtn = document.getElementById('nextButton');
+    let intervalId = null;
+
+    // Restorre the auto-play state from localStorage
+    if (localStorage.getItem('autoPlay') === 'true') {
+        autoPlayCheckbox.checked = true;
+    }
+
+    // Auto-play function
+    function enableAutoPlay() {
+        if (video) {
+            video.onended = () => nxtBtn.click();
+        } else {
+            intervalId = setInterval(() => {
+                console.log('Next number is ...');
+                nxtBtn.click();
+            }, 10000);
+        }
+    }
+
+    //if the checkbox is checked, enable auto-play
+    if (autoPlayCheckbox.checked) enableAutoPlay();
+
+    // Save the auto-play state to localStorage for any changes
+    autoPlayCheckbox.addEventListener('change', function(){
+        if (this.checked) {
+            console.log('Auto-play on!');
+            localStorage.setItem('autoPlay', this.checked);
+            enableAutoPlay();
+        } else {
+            console.log('Auto-play off!')
+            if (video) video.onended = null;
+            clearInterval(intervalId);
+            localStorage.setItem('autoPlay', false);
+        }
+    });
+
+});
